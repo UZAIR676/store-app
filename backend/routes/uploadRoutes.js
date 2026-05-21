87f47a95d8e000e502afb7620ac2981d27de21bd
uzair rana <you@example.com>
2026-05-21 01:20:47 -0400

    images update

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -39,7 +39,7 @@ router.post("/", (req, res) => {
     } else if (req.file) {
       res.status(200).send({
         message: "Image uploaded successfully",
-        image: `/${req.file.path}`,
+        image: `https://store-app-production-00d2.up.railway.app/uploads/${req.file.filename}`,
       });
     } else {
       res.status(400).send({ message: "No image file provided" });
